d018c81130eca50fff987148971463fb8a633759
Javier Parra <huevoos@gmail.com>
2018-10-08 20:36:37 -0500

    Docker compose with database.

--- server/python/src/app/app.py
+++ server/python/src/app/app.py
@@ -1,12 +1,23 @@
+from pprint import pprint
 import falcon
+
 
 class Todos():
     def __init__(self):
         pass
 
     def on_get(self, req, resp):
+        todo = {
+            "id": 1,
+            "complete": True,
+            "name": "Take out the trash",
+            "created": 1455151212,
+            "completed": 1455171642
+        }
+        print(todo)
         resp.status = falcon.HTTP_200
-        resp.body = 'Hola mundo'
+        resp.body = 'Holas mundos'
+
 
 todos = Todos()
 
